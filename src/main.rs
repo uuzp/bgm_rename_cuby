3425c27230d405eb65d24ff1ff1c1c54b216ba7c
pheme <a@ying.ws>
2022-09-05 23:16:14 +0900

    0.3.4 重写代码&&GUI界面，初步实现批量处理功能。

--- src/main.rs
+++ src/main.rs
@@ -1,344 +1,186 @@
-#![windows_subsystem = "windows"]
-use fltk::{app, browser, button::Button, dialog, input, prelude::*, window::Window};
-use jsonpath_lib as jsonpath;
-use open;
-use reqwest::{self, header::USER_AGENT};
-use serde_json;
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::{
-    cell::RefCell,
-    cmp::Ordering,
-    env,
-    error::Error,
-    fs, io,
-    path::{Path, PathBuf},
-    rc::Rc,
-};
+use std::{cell::RefCell, rc::Rc, path::PathBuf};
 
-struct BgmData {
-    name: Vec<String>,
-    url: Vec<String>,
-}
-struct Ep {
-    name: Vec<String>,
-    year: String,
-    //    total: i8,
-}
-struct Files {
-    video: Vec<PathBuf>,
-    sub: Vec<PathBuf>,
-    sub_sc: Vec<PathBuf>,
-    sub_tc: Vec<PathBuf>,
-}
+use fltk::{app, prelude::{WidgetExt, BrowserExt, WidgetBase, GroupExt, InputExt, WindowExt}, window::{WindowType, Window}, browser::MultiBrowser, input::Input, button::Button,dialog, image};
+use cuby::{Msg, Bgm};
 
-impl BgmData {
-    fn new(keywords: String) -> Result<BgmData, Box<dyn Error>> {
-        let url = format!(
-            "https://api.bgm.tv/search/subject/{}?type=2&responseGroup=small",
-            keywords
-        );
-        let json_body: serde_json::Value = reqwest::blocking::get(url)?.json()?;
 
-        let select = |x: &str| {
-            let mut v = Vec::new();
-
-            for s in jsonpath::selector(&json_body)(x).unwrap() {
-                v.push(s.as_str().unwrap().to_string());
-            }
-            v
-        };
-
-        let name_cn = select("$.list.*.name_cn");
-        let url = select("$.list.*.url");
-        let name = {
-            let mut v = Vec::new();
-            for i in 0..url.len() {
-                let s = match name_cn[i].len() {
-                    0 => jsonpath::selector(&json_body)("$.list.*.name").unwrap()[i]
-                        .as_str()
-                        .unwrap()
-                        .to_string(),
-                    _ => name_cn[i].to_owned(),
-                };
-                v.push(s);
-            }
-            v
-        };
-
-        //TODO 或许还可以优化逻辑，争取只创建一次vec。
-
-        Ok(BgmData { name, url })
-    }
-}
-impl Ep {
-    fn new(bgm_id: &str) -> Result<Ep, Box<dyn Error>> {
-        let client = reqwest::blocking::Client::new();
-        let url = format!(
-            "https://api.bgm.tv/v0/episodes?subject_id={}&type=0&limit=100&offset=0",
-            bgm_id
-        );
-
-        let json_body: serde_json::Value = client
-            .get(url)
-            .header(USER_AGENT, "bgm_rename_rust_cuby")
-            .send()?
-            .json()?;
-        let mut ep_list = Vec::new();
-        let year = jsonpath::select(&json_body, "$.data.*.airdate").unwrap();
-
-        let year = year[0].as_str().unwrap()[0..4].to_string();
-        // let total = jsonpath::select(&json_body, "$.total").unwrap();
-        // let total = total[0].as_i64().unwrap();
-        // let total = total as usize;
-
-        let mut epn = Vec::new();
-        for s in jsonpath::selector(&json_body)("$.data.*.sort").unwrap() {
-            epn.push(s.as_i64().unwrap());
-        }
-        for s in jsonpath::selector(&json_body)("$.data.*.name_cn").unwrap() {
-            ep_list.push(s.as_str().unwrap());
-        }
-        let mut name = vec![];
-        for i in 0..ep_list.len() {
-            let ep = match epn[i].cmp(&10) {
-                Ordering::Less => "ep0",
-                _ => "ep",
-            };
-            let epl = format!("{}{} - {}", ep, epn[i], ep_list[i]);
-            name.push(epl);
-        }
-        let ep = Ep { name, year };
-
-        Ok(ep)
-    }
-}
-impl Files {
-    fn new(root: &Path) -> io::Result<Files> {
-        let mut sub = vec![];
-        let mut sub_sc = vec![];
-        let mut sub_tc = vec![];
-        let mut video = vec![];
-        let videos = "mp4 mkv";
-        let subs = "ass str";
-        let sc = "chs sc gb";
-        let tc = "cht tc big5";
-
-        for path in fs::read_dir(root).unwrap() {
-            let path = path.unwrap().path();
-
-            let ts1 = match path.to_str().unwrap().rsplit_once(".") {
-                Some(x) => x,
-                None => ("None", "None"),
-            };
-
-            let ts = match ts1.0.rsplit_once(".") {
-                Some((_x, y)) => y,
-                None => "None",
-            };
-            let suf = ts1.1;
-
-            match subs.contains(suf) {
-                true => match sc.contains(&ts.to_lowercase()) {
-                    true => sub_sc.push(path.to_owned()),
-                    false => match tc.contains(&ts.to_lowercase()) {
-                        true => sub_tc.push(path.to_owned()),
-                        false => sub.push(path.to_owned()),
-                    },
-                },
-                false => match videos.contains(suf) {
-                    true => video.push(path.to_owned()),
-                    false => (),
-                },
-            }
-        }
-        let files = Files {
-            video,
-            sub,
-            sub_sc,
-            sub_tc,
-        };
-
-        Ok(files)
-    }
-}
-
-fn select_folder() -> String {
-    let mut dialog = dialog::NativeFileChooser::new(dialog::NativeFileChooserType::BrowseDir);
-    dialog.show();
-    dialog.filename().as_path().display().to_string()
-}
-fn main() -> Result<(), Box<dyn Error>> {
+fn main() {
+    // GUI
     let app = app::App::default();
-    let mut window = Window::new(100, 100, 850, 600, "Cuby");
+    // 主窗口
+    let mut w = Window::new(602, 502, 820, 320, "cuby");
+    w.set_type(WindowType::Normal);
+    w.set_label_size(20);
+    // 番剧源文件夹路径列表
+    let mut br1 = MultiBrowser::new(50, 50, 500, 260, None);
+    let widths = &[260,40,200];
+    br1.set_column_widths(widths);
+    br1.set_column_char('\t');
+    // 搜索列表
+    let mut br2 = MultiBrowser::new(600, 50, 200, 200, None);
+    // 目标文件夹路径
+    let mut i1 = Input::new(50, 10, 500, 25, None);
+    // 搜索栏
+    let i2 = Input::new(600, 10, 160, 25, None);
+    // 按钮
+    let mut b1 = Button::new(10, 10, 25, 25, "➕");
+    let mut b2 = Button::new(10, 50, 25, 25, "➕");
+    let mut b3 = Button::new(10, 75, 25, 25, "➖");
+    let mut b4 = Button::new(555, 50, 40, 25, "<=");
+    b4.set_label_size(20);
+    let mut b5 = Button::new(775, 10, 25, 25, "🔎");
+    let mut b6 = Button::new(600, 260, 70, 50, "Link");
+    let mut b7 = Button::new(700, 260, 100, 50, "Start");
+    // 主窗口绘制结束
+    w.end();
+    // 显示主窗口
+    w.show();
+    // 设置窗口图标
+    let a = cuby::get_png();
+    let img = image::PngImage::from_data(a.data.as_ref()).unwrap();
+    w.set_icon(Some(img));
+    
+    // 消息传递
+    let (s,r) = app::channel::<Msg>();
+    b1.emit(s, Msg::AddOut);
+    b2.emit(s, Msg::AddIn);
+    b3.emit(s, Msg::RemoveOut);
+    b4.emit(s, Msg::Set);
+    b5.emit(s, Msg::Search);
+    b6.emit(s, Msg::Link);
+    b7.emit(s, Msg::Start);
+    
 
-    let sf = input::Input::new(120, 110, 700, 25, "");
-    let sf = Rc::new(RefCell::new(sf));
-    let ef = input::Input::new(120, 160, 700, 25, "");
-    let ef = Rc::new(RefCell::new(ef));
+    // 存储数据，v1存储path-out，v2存储path-in's,v3存储bgms
+   // let v1:Rc<RefCell<Vec<PathBuf>>> = Rc::new(RefCell::new(Vec::new()));
+    let v2:Rc<RefCell<Vec<PathBuf>>> = Rc::new(RefCell::new(Vec::new()));
+    let v3:Rc<RefCell<Vec<(String,String)>>> = Rc::new(RefCell::new(Vec::new()));
 
-    let mut sf_btn = Button::new(20, 100, 80, 40, "源路径");
-    let mut ef_btn = Button::new(20, 150, 80, 40, "输出路径");
+    // 临时数据
+    let v4:Rc<RefCell<Vec<Bgm>>> = Rc::new(RefCell::new(Vec::new()));
+    
+    // 消息接收
+    while app.wait() {
+        if let Some(msg) = r.recv(){
+            match msg {
+                Msg::AddOut => {
+                    i1.set_value(cuby::select_folder().to_str().unwrap());
 
-    let search = input::Input::new(100, 230, 500, 25, "");
-    let mut search_btn = Button::new(650, 220, 80, 40, "搜索");
+                }
+                Msg::AddIn => {
+                    let p = cuby::select_folder();
+                    let p = match p.to_str() {
+                        None => continue,
+                        Some(_) => p,
+                    }; 
+                    br1.add(&p.to_str().unwrap().to_string());
+                    v2.borrow_mut().push(p);
 
-    let mut list_title = browser::Browser::new(20, 280, 700, 20, "");
-    list_title.set_column_widths(&[50, 10, 400, 10, 100]);
-    list_title.set_column_char('\t');
-    list_title.add("@c序号\t@c|\t@c番剧名\t@c|\t@cURL");
-    let mut list = browser::MultiBrowser::new(20, 300, 700, 280, "");
+                }
+                Msg::RemoveOut => {
+                    let line = br1.value();
+                    br1.remove(line);
+                    v2.borrow_mut().remove((line as usize)-1);
 
-    list.set_column_widths(&[50, 10, 400, 10, 100]);
-    list.set_column_char('\t');
+                }
+                Msg::Set => {
+                    match br2.value() > 0 {
+                        true => (),
+                        false => {
+                            dialog::message_default("未选择番剧！");
+                            continue;
+                        }
+                    }
+                    let n = (br2.value() as usize)-1;
+                    let m = (v4.borrow()[0].id[n].clone(),v4.borrow()[0].name[n].clone());
+                    v3.borrow_mut().push(m.clone());
+                    let eq = v3.borrow().len() > v2.borrow().len();
+                    match eq  {
+                        true => {
+                            dialog::message_default("未选择番剧文件夹！");
+                            v3.borrow_mut().pop();
+                            continue;
+                        },
+                        false => (),
+                    };
+                    v4.borrow_mut().clear();
+                    br1.clear();
+                    br2.clear();
+                    let len = v3.borrow().len();
+                    for i in 0..len{
+                        let s = format!("{}\t => \t{}",v2.borrow()[i].to_str().unwrap(),v3.borrow()[i].1);
+                        br1.add(&s);
+                    }
 
-    let list = Rc::new(RefCell::new(list));
+                    for s in v2.borrow().iter().skip(len) {
+                        br1.add(&s.to_str().unwrap());
+                    }
 
-    let mut url_btn = Button::new(750, 400, 80, 40, "前往URL");
-    let mut start_btn = Button::new(750, 500, 80, 40, "开始");
+                }
+                Msg::Search => {
+                    let keywords = i2.value();
+                    let bgm = Bgm::new().get(&keywords);
+                    
+                    v4.borrow_mut().clear();
+                    v4.borrow_mut().push(bgm.clone());
+                    br2.clear();
+                    for s in bgm.name {
+                        br2.add(&s)
+                    }
 
-    window.end(); //窗口绘制停止
-    window.show();
-    let sf1 = Rc::clone(&sf);
-    let ef1 = Rc::clone(&ef);
-    sf_btn.set_callback(move |_| {
-        sf1.borrow_mut().set_value(&select_folder());
-    });
-    ef_btn.set_callback(move |_| {
-        ef1.borrow_mut().set_value(&select_folder());
-    });
+                    
 
-    search_btn.set_callback(move |_| {
-        //dialog::message_default(&search.value());
-        let keywords = search.value();
+                }
+                Msg::Link => {
+                    //TODO v3是番剧名得和v1的输出路径合并，v2是输入文件夹，得读取每个文件夹里的视频和字幕文件，用for循环。然后 link v1+v2 => v3+Ep.name。
+                    let path = cuby::set_out_path(&i1.value());
+                    // 错误处理，防止输出文件夹路径为空
+                    let path = match path.to_str().unwrap().is_empty() {
+                        true => {
+                            dialog::message_default("未设置输出文件夹！");
+                            continue;
+                        },
+                        false => path,
+                    };
+                    println!("path:\n{:?}\n",path);
+                    let v_names:Vec<String> = v3.borrow().iter().map(|x| x.1.clone() ).collect();
+                    let v_inpaths:Vec<PathBuf> = v2.borrow().iter().map(|x| x.clone()).collect();
+                    let v_outpaths = cuby::mkoutpaths(path, v_names);
+                    cuby::mkdir(&v_outpaths);
+                    cuby::name_extension(v_inpaths,v_outpaths);
+                   
 
-        let bgm = BgmData::new(keywords).unwrap();
+                    br1.clear();
+                    v2.borrow_mut().clear();
+                    v3.borrow_mut().clear();
 
-        list.borrow_mut().clear();
-        for i in 0..bgm.name.len() {
-            // let s = match bgm.name_cn[i].len() {
-            //     0 => format!("@c{}\t|\t{}\t|\t{}", i, bgm.name[i], bgm.url[i]),
-            //     _ => format!("@c{}\t|\t{}\t|\t{}", i, bgm.name_cn[i], bgm.url[i]),
-            // };
-            let s = format!("@c{}\t|\t{}\t|\t{}", i, bgm.name[i], bgm.url[i]);
-            list.borrow_mut().add(&s);
+                }
+                Msg::Start => {
+                    let path = cuby::set_out_path(&i1.value());
+                    // 错误处理，防止输出文件夹路径为空
+                    let path = match path.to_str().unwrap().is_empty() {
+                        true => {
+                            dialog::message_default("未设置输出文件夹！");
+                            continue;
+                        },
+                        false => path,
+                    };
+                    let v_names= v3.borrow().clone();
+                   // let v_inpaths:Vec<PathBuf> = v2.borrow().iter().map(|x| x.clone()).collect();
+                    let v_inpaths = v2.borrow().clone();
+                    let (v_ep,v_outpaths) = cuby::out_ep_path(path, v_names);
+                    cuby::tolink(v_inpaths , v_outpaths, v_ep);
+                    br1.clear();
+                    v2.borrow_mut().clear();
+                    v3.borrow_mut().clear();
+                }
+                
+            }
         }
-
-        let sf2 = Rc::clone(&sf);
-        let ef2 = Rc::clone(&ef);
-
-        let list1 = Rc::clone(&list);
-        let list2 = Rc::clone(&list);
-        let bgm_url = bgm.url.clone();
-        //URL按钮
-        url_btn.set_callback(move |_| {
-            let url = &bgm_url[(list1.borrow().value() - 1) as usize];
-            open::that(url).unwrap();
-        });
-        //开始按钮
-        start_btn.set_callback(move |_| {
-            let key = (list2.borrow().value() - 1) as usize;
-
-            let bgm_name = &bgm.name[key];
-            let str = format!("确认<{}>无误？", bgm_name);
-
-            let choice = dialog::choice2_default(&str, "No", "Yes", "");
-
-            if let Some(1) = choice {
-                let bgm_id = &bgm.url[key].rsplit_once("/").unwrap().1;
-
-                let ep = Ep::new(bgm_id).unwrap();
-
-                let val1 = sf2.borrow().value();
-                let path1 = Path::new(&val1);
-                let mut path2 = PathBuf::new();
-
-                let val2 = ef2.borrow().value();
-
-                path2.push(val2);
-                let bgm_name = bgm_name.replace("/", "／");
-                let bgm_name = bgm_name.replace(":", "：");
-                path2.push(format!("{} ({})", bgm_name, ep.year));
-                let path2 = Path::new(&path2);
-
-                match fs::create_dir(path2) {
-                    Ok(floder) => floder,
-                    Err(err) => {
-                        let err = format!("{}", err);
-                        dialog::message_default(&err);
-                    }
-                };
-                assert!(env::set_current_dir(path2).is_ok()); //设置工作目录
-
-                let files = Files::new(path1).unwrap();
-
-                //设定一个开始的剧集数,一个输入框、
-                let start = 0;
-
-                match files.video.len() {
-                    0 => (),
-                    _ => {
-                        let max = files.video.len();
-                        for i in start..max {
-                            let video = format!(
-                                "{}.{}",
-                                ep.name[i],
-                                &files.video[i].extension().unwrap().to_string_lossy()
-                            );
-                            assert!(fs::hard_link(&files.video[i], video).is_ok());
-                        }
-                    }
-                }
-
-                match files.sub.len() {
-                    0 => (),
-                    _ => {
-                        let max = files.sub.len();
-                        for i in start..max {
-                            let sub = format!(
-                                "{}.{}",
-                                ep.name[i],
-                                &files.sub[i].to_str().unwrap().split_once(".").unwrap().1
-                            );
-                            assert!(fs::copy(&files.sub[i], sub).is_ok());
-                        }
-                    }
-                }
-
-                match files.sub_sc.len() {
-                    0 => (),
-                    _ => {
-                        let max = files.sub_sc.len();
-                        for i in start..max {
-                            let sub_sc = format!(
-                                "{}.{}",
-                                ep.name[i],
-                                &files.sub_sc[i].to_str().unwrap().split_once(".").unwrap().1
-                            );
-                            assert!(fs::copy(&files.sub_sc[i], sub_sc).is_ok());
-                        }
-                    }
-                }
-
-                match files.sub_tc.len() {
-                    0 => (),
-                    _ => {
-                        let max = files.sub_tc.len();
-                        for i in start..max {
-                            let sub_tc = format!(
-                                "{}.{}",
-                                ep.name[i],
-                                &files.sub_tc[i].to_str().unwrap().split_once(".").unwrap().1
-                            );
-                            assert!(fs::copy(&files.sub_tc[i], sub_tc).is_ok());
-                        }
-                    }
-                }
-
-                dialog::message_default("成功！");
-            };
-        });
-    });
-
-    app.run().unwrap();
-    Ok(())
+        
+    }
+   
+    
 }
